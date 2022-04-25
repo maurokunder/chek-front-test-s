6ad90bcea85106f75691183c7bbf9878fc786239
maurokunder <moyarzun@kunder.cl>
2022-04-25 16:52:08 +0100

    install flexlayaout and angular material dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { GlobalHeaderComponent } from './components/global-header/global-header.component';
 import { NewRecipientComponent } from './components/new-recipient/new-recipient.component';
 import { TransferComponent } from './components/transfer/transfer.component';
 import { TransferHistoryComponent } from './components/transfer-history/transfer-history.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { TransferHistoryComponent } from './components/transfer-history/transfer
     TransferHistoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
